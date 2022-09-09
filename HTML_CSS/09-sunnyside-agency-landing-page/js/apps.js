ca70dcfe232666f0d149e3367d622231e33d795f
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-09-09 16:24:32 +0200

    peleando con js

--- HTML_CSS/09-sunnyside-agency-landing-page/js/apps.js
+++ HTML_CSS/09-sunnyside-agency-landing-page/js/apps.js
@@ -1,6 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const menuContent = document.querySelector(".menu-content");
 
-hamburger.addEventListener("click", () => {
-  menuContent.classList.toggle("active");
-});
+// const hamburger = document.querySelector(".hamburger");
+// const menuContent = document.querySelector(".menu-content");
+
+// hamburger.addEventListener("click", () => {
+//   menuContent.classList.toggle("active");
+// });
+
+var hamburguesa = document.getElementsByClassName('.hamburger');
+hamburguesa.style.display = 'none';
---
+++ js/mostrar y ocultar contenido/js/apps.js
@@ -0,0 +1,9 @@
+function mostrarPremio() {
+  document.getElementById('premio').style.display = 'block';
+  document.getElementById('triste').style.display = 'none';
+}
+
+function ocultarPremio() {
+  document.getElementById('premio').style.display = 'none';
+  document.getElementById('triste').style.display = 'block';
+}
