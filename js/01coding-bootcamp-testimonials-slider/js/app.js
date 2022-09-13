a7d9a458c8db768c16f7dd0b6b5be37fb11dd473
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-09-13 21:16:47 +0200

    Subida de javaScript de Frontend Mentor: Coding Bootcamp Testimonials slider modified:   js/01coding-bootcamp-testimonials-slider/js/app.js

--- js/01coding-bootcamp-testimonials-slider/js/app.js
+++ js/01coding-bootcamp-testimonials-slider/js/app.js
@@ -1,7 +1,7 @@
 const btns = document.querySelectorAll('.toggler img');
 const imagen = document.querySelectorAll('.img');
 const testimonio = document.querySelectorAll('.card-content');
-
+console.log(imagen);
 // console.log(btns);
 
 btns.forEach((btn) => {
@@ -10,17 +10,24 @@ btns.forEach((btn) => {
     const index = e.target.classList.contains('prev') ? -1 : 1;
     const activeImg = document.querySelector('.img.active');    
     const activeCardContent = document.querySelector('.card-content.active');    
-    const nextIndex = [...imagen].indexOf(activeImg) + index;
+    let nextIndex = [...imagen].indexOf(activeImg) + index;
     // console.log([imagen].indexOf(activeImg));
     // console.log(imagen.length);
       console.log(nextIndex);
     // console.log(index);
     if (nextIndex < 0) {
-      nextIndex = imagen.length - 1;      
+      nextIndex = (imagen.length - 1);      
     }
     if (nextIndex >= imagen.length) {
       nextIndex = 0;      
     }
+
+    imagen[nextIndex].classList.add('active');
+    testimonio[nextIndex].classList.add('active');
+    
+    activeImg.classList.remove('active');
+    activeCardContent.classList.remove('active');
+    
     console.log(nextIndex);
   });
 });
