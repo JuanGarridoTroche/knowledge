66ea2912b2f17a9954ef35122ebdbc381bf77352
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-09-21 15:10:08 +0200

    Changes to be committed: modified:   "js/Men\303\272 desplegable/css/style.css" modified:   "js/Men\303\272 desplegable/index.html" modified:   "js/Men\303\272 desplegable/js/main.js"

--- js/Menú desplegable/js/main.js
+++ js/Menú desplegable/js/main.js
@@ -1,12 +1,11 @@
-let listElements = document.querySelectorAll('.list-button-click');
+const listElements = document.querySelectorAll('.list-button-click');
 
 listElements.forEach(elemento => {
-  elemento.addEventListener('click', ()=>{
-    // alert();
+  elemento.addEventListener('click', ()=>{    
     elemento.classList.toggle('arrow');
     let height = 0;
     let menu = elemento.nextElementSibling;
-    // console.log(menu);
+    console.log(menu);
     console.log(menu.scrollHeight);
     if (menu.clientHeight == 0) {
       height = menu.scrollHeight;
