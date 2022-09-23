2ec16d037132cb55bf4cf8936c96b7f6644fe599
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-09-23 16:40:23 +0200

    Changes to be committed: modified:   "js/Men\303\272 desplegable horizontal/js/App.js"

--- js/Menú desplegable horizontal/js/App.js
+++ js/Menú desplegable horizontal/js/App.js
@@ -6,6 +6,7 @@ listaDeElementos.forEach(elemento => {
   elemento.addEventListener('click', ()=> {
     elemento.classList.toggle('arrow');
     let height = 0;
+    let altura = 0;
     let menu = elemento.nextElementSibling;
     if (menu.clientHeight == 0) {
       height = menu.scrollHeight;
@@ -14,8 +15,10 @@ listaDeElementos.forEach(elemento => {
       height = 0;
       menu.style.position = 'unset';
     }
-
+    altura = height + 10;
     menu.style.height = `${height}px`;
-    
+    console.log(altura);
+    console.log(height);
   });
+  
 });
