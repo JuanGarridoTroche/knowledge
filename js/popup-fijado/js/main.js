cf0de29d4f478209f782da37f4896f183c3292a6
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-11-22 23:59:10 +0100

    popup fixed: creación de ficheros

---
+++ js/popup-fijado/js/main.js
@@ -0,0 +1,11 @@
+
+
+const anaAvatar = document.querySelector("#ana");
+const nikyAvatar = document.querySelector("#niky");
+const juanAvatar = document.querySelector("#juan");
+
+console.log(juanAvatar);
+
+juanAvatar.addEventListener("click", ()=> {
+  console.log("Juan Garrido Troche");
+});
