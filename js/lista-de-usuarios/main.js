9eff242ed59e156aa070016261a0a0fac1efb9ea
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-12-04 17:26:27 +0100

    Lista de usuarios: Promise, .then y Async con await

---
+++ js/lista-de-usuarios/main.js
@@ -0,0 +1,35 @@
+const contactBox = document.querySelector('#contactBox');
+const loadUsersBox = document.querySelector('#loadUsersBox');
+const loadUsersBtn = document.querySelector('#loadUsersBtn');
+
+loadUsersBtn.addEventListener('click', getUsersData);
+
+function getUsersData() {
+  toggleContactBox();
+  asyncFetching();
+}
+
+function toggleContactBox() {
+  contactBox.classList.toggle('hidden');
+  loadUsersBox.classList.toggle('hidden');
+}
+
+//Usaremos una API-REST req-res (https://reqres.in). Podemos hacer pruebas con listas de usuarios fake que nos entrega esta API en vez de perder tiempo creándolos uno mismo.
+
+/* Se podría realizar con fetch pero para al no controlar el tiempo de consulta de un servicio exterior como es la API, debemos utilizar una Promise con async y await:
+fetch('https://reqres.in/api/users')
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+*/
+//
+const asyncFetching = async() => {
+  const response = await fetch('https://reqres.in/api/users');
+  if (response.status!==200) {
+    throw new Error('Incapaz de obtener datos 😿')
+  }
+  const { data, ...rest } = await response.json();
+  console.log(rest);
+  console.log(data);
+  return data;
+}
+
