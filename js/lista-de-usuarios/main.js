3610e4dec11b589ce358a2f2d64987f4dce5aab7
Juan Garrido Troche <j.garridotroche@gmail.com>
2022-12-04 23:17:37 +0100

    Lista de usuarios: Promise, .then y Async con await

--- js/lista-de-usuarios/main.js
+++ js/lista-de-usuarios/main.js
@@ -7,6 +7,8 @@ const usersNumber = document.querySelector('#usersNumber');
 const userList = document.querySelector('#userList');
 const cardSkeleton = document.querySelector('#cardSkeleton');
 
+const cardTemplate = document.querySelector('#cardTemplate');
+
 loadUsersBtn.addEventListener('click', getUsersData);
 
 function getUsersData() {
@@ -16,10 +18,38 @@ function getUsersData() {
     toggleContactBox();
     printNumberOfUsers(data);
     printSkeleton(data);
+    userList.innerHTML = '';
+    printUserData(data);
   })
   .catch((error) => {
     console.error(error);
   });
+};
+
+function printUserData(data) {
+  /* Sacamos un user del console log y lo copiamos para poder realizar un destructuring correcto
+  const user = {
+    avatar: "https://reqres.in/img/faces/1-image.jpg",
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    id: 1,
+    last_name: "Bluth"
+  } 
+  */
+  data.forEach(user => {
+    const {avatar, email, first_name, id, last_name} = user;
+    // console.log(user.email);
+    const cardContent = cardTemplate.content.cloneNode(true);
+    const avatarImg = cardContent.querySelector('#avatarImg');
+    avatarImg.src = avatar;
+
+    const firstName = cardContent.querySelector('#firstName');
+    firstName.textContent = first_name;
+    const lastName = cardContent.querySelector('#lastName');
+    lastName.textContent = last_name;
+
+    userList.appendChild(cardContent);
+  })
 }
 
 function printSkeleton(data) {
@@ -48,7 +78,8 @@ fetch('https://reqres.in/api/users')
 */
 //
 const asyncFetching = async() => {
-  const response = await fetch('https://reqres.in/api/users');
+  //En la URL le añadimos un delay de 2s para que le de tiempo a comprobar si tiene que cargar el skeleton (0 usuarios) o los usuarios. Si no lo queremos añadir solo tenemos que eliminar de la URL "?delay=2"
+  const response = await fetch('https://reqres.in/api/users?delay=2');
   if (response.status!==200) {
     throw new Error('Incapaz de obtener datos 😿')
   }
